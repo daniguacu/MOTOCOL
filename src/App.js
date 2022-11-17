b04304f973b2eace7779ef6f885a2d624a155232
daniguacu <daniguacu@gmail.com>
2022-11-16 20:39:37 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Booking from "./components/booking/Booking"
+import Formulario from "./components/formulario/Formulario"
+import Header from "./components/header/Header"
+
+
+import{useState}from "react"
+
+const initinfo={
+  nombre:"",
+  apellido:"",
+  email:""
+}
 function App() {
+  
+  const[moto,bookmoto]=useState(false)
+  const[cambiodeusuario,setcambiodeusuario]=useState(false)
+  const [count,setcount]=useState(0)  
+  const[newcount,setnewcount]=useState(0)
+  const[inputinfo,setinputinfo]=useState(initinfo)
+  const[disponible,setdisponible]=useState(8)
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Header></Header>
+        <Booking bookmoto={bookmoto} setcount={setcount} count={count} cambiodeusuario={cambiodeusuario} setcambiodeusuario={setcambiodeusuario} newcount={newcount} setnewcount={setnewcount}disponible={disponible} ></Booking>
+        <Formulario count={count} setinputinfo={setinputinfo} inputinfo={inputinfo} setcambiodeusuario={setcambiodeusuario} newcount={newcount} setdisponible={setdisponible} ></Formulario>
+        
+      </div>
+    </>
   );
 }
 
